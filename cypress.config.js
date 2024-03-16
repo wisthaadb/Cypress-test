e4f70fdaa185d05ee2b179fee3ac7f3030142a0f
wisthaadb <wistha.adhika@gmail.com>
2024-03-16 22:26:48 +0700

    config, package, download test

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
+         on('task', {downloadFile})
+      }
+    }
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import 'cypress-file-upload';
+
+require('cypress-downloadfile/lib/downloadFileCommand')
